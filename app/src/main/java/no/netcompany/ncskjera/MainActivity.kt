0c52139597eba612b634d8f4e22dbb13a6f71802
Kristoffer Lervik <kristl@netcompany.com>
2018-12-10 17:43:38 +0100

    BE Lager liste

--- app/src/main/java/no/netcompany/ncskjera/MainActivity.kt
+++ app/src/main/java/no/netcompany/ncskjera/MainActivity.kt
@@ -1,39 +1,66 @@
 package no.netcompany.ncskjera
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Initializing an empty ArrayList to be filled with animals
+    private val animals: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        // Loads animals into the ArrayList
+        addAnimals()
+
+        // Creates a vertical Layout Manager
+        rv_animal_list.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        rv_animal_list.adapter = AnimalAdapter(animals, this)
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    // Adds animals to the empty animals ArrayList
+    fun addAnimals() {
+        animals.add("dog")
+        animals.add("cat")
+        animals.add("owl")
+        animals.add("cheetah")
+        animals.add("raccoon")
+        animals.add("bird")
+        animals.add("snake")
+        animals.add("lizard")
+        animals.add("hamster")
+        animals.add("bear")
+        animals.add("lion")
+        animals.add("tiger")
+        animals.add("horse")
+        animals.add("frog")
+        animals.add("fish")
+        animals.add("shark")
+        animals.add("turtle")
+        animals.add("elephant")
+        animals.add("cow")
+        animals.add("beaver")
+        animals.add("bison")
+        animals.add("porcupine")
+        animals.add("rat")
+        animals.add("mouse")
+        animals.add("goose")
+        animals.add("deer")
+        animals.add("fox")
+        animals.add("moose")
+        animals.add("buffalo")
+        animals.add("monkey")
+        animals.add("penguin")
+        animals.add("parrot")
     }
 }
